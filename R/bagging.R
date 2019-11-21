15e82a753fdd3cda04bd7dd8815b0896d8ee0327
Bowen Lei <bowen.lei@stat.tamu.edu>
2019-11-21 13:55:42 -0600

    Add the bagging function that implement the algorithm of Bagging.

--- R/bagging.R
+++ R/bagging.R
@@ -24,7 +24,33 @@ bagging_fit1 <- function(fweak, data){
 }
 
 #' Function that implement the algorithm of Bagging
-
-bagging <- function(){
-
+#'
+#' @param fweak - function that generates estimate from weak model based on input
+#' @param data - list of data that fweak need
+#' @param model_num - the number of weak models you want to train and combine
+#' @param reg - logical value, true if the weak model is doing regression, otherwise it's doing classification
+#'
+#' @return outputs bagging_fit1(fweak, data)
+#' @export
+#'
+#' @examples
+#' fweak <- function(x, y){
+#'   lm(y ~ x)$coefficients
+#' }
+#' data <- list(x = matrix(rnorm(1000), 200, 5))
+#' data$y <- data$x %*% rnorm(5)
+#' bagging_fit1(fweak, data)
+bagging <- function(fweak, data, model_num, reg){
+  multi_est <- list()
+  length(multi_est) <- model_num
+  for(i in 1:model_num){
+    multi_est[[i]] <- bagging_fit1(fweak, data)
+  }
+  Comb_parallel(multi_est, rep(1, model_num), reg)
 }
+
+
+
+
+
+
