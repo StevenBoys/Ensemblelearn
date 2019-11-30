afe6cd3dff580b362ccd4ad148132cf79fd5cbe4
Bowen Lei <bowen.lei@stat.tamu.edu>
2019-11-30 01:07:04 -0600

    Change the form of output in bagging_fit1 function.

--- R/bagging.R
+++ R/bagging.R
@@ -1,4 +1,4 @@
-#' Function that implement one resample of Bagging
+#' Function that implement one resample of Bagging in regression
 #'
 #' @param fweak - function that generates estimate from weak model based on input
 #' @param data - list of data that fweak need
@@ -20,7 +20,13 @@ bagging_fit1 <- function(fweak, data){
   data$y <- data$y[index_resample]
   data$x <- data$x[index_resample, ]
   # Fit the weak model based on the resampled data
-  fit_model(fweak, T, data)
+  coef <- fit_model(fweak, T, data)
+  # Construct the trained model based on the coef
+  model_train <- function(x){
+    x %*% coef
+  }
+  # Returen the trained function
+  model_train
 }
 
 #' Function that implement the algorithm of Bagging
@@ -50,7 +56,7 @@ bagging <- function(fweak, data, model_num, reg){
     model_train[[i]] <- bagging_fit1(fweak, data)
   }
   # Get the multiple estimation based on the trained models
-  multi_est <- prediction(data$x, model_train, reg, parallel = T)
+  multi_est <- prediction(data$x, model_train, parallel = T)
   # Combine the multiple estimation
   comb_out <- Comb_parallel(multi_est, rep(1, model_num), reg)
   # Return the fitted values on training data and the list of weak models
